63c944629c24ec4f9c5c57d77fa51b2bffb32a84
pastier <patrickstierle@web.de>
2017-08-31 14:07:50 +0200

    Added installation guide to readme and documentation to app settings

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # SmartMirror
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.3.2.
+This is custom made Frontend for a Raspberry PI SmartMirror Project using Angular2.
 
 ## Angular2 Frontend
 
-This is custom made Frontend for a Raspberry PI SmartMirror Project
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.3.2.
+
+## TODO
+
+Make background black and text white to work with SmarMirror.
+
+# Installation guide
+
+If you want to use this Frontend for your SmartMirror you can download this Project.
+You need Nodejs and Angular CLI.
+Steps to start:
+<p>1.Goto src/app/app.settings.ts and enter your credentials</p>
+<p>2.Open terminal in SmartMirror/</p>
+<p>3.npm start</p>
+<p>4.Open browser at localhost:4200</p>
--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,18 +1,32 @@
 export class AppSettings {
 
-    // API KEYS
+    // API KEYS - enter your OpenWeatherMap api key here.
     public static OPEN_WEATHER_MAP_API = 'API_KEY';
 
+    // enter your google calendar clientId here - i used this link to create it https://console.developers.google.com/start/api?id=calendar.
     public static GOOGLE_CALENDAR_API = 'API_KEY';
 
-    // REQUEST URLS
+    // REQUEST URLS - enter the urls for OpenWeatherMap api you want to request, examples and explanation below.
+    // OpenWeatherMap api documentation https://openweathermap.org/api
+    // http://api.openweathermap.org/data/2.5/weather?id=2815076&units=metric&lang=de, i used german, metric system and my hometown to be displayed.
     public static OPEN_WEATHER_MAP_URL1 = 'currentweather';
+    // needed the same request in english to cast the OpenWeatherMap icons to weather-icons.
     public static OPEN_WEATHER_MAP_URL2 = 'currentweather_eng';
+    // http://api.openweathermap.org/data/2.5/forecast/daily?id=2815076&units=metric&lang=de&cnt=5, same as current, with 5 days forecast.
     public static OPEN_WEATHER_MAP_URL3 = 'forecastweather';
+    // needed the same request in english to cast the OpenWeatherMap icons to weather-icons.
     public static OPEN_WEATHER_MAP_URL4 = 'forecastweather_eng';
 
+    // enter the news feed you want in here.
+    // i used this online api to convert the news rss xml to json https://api.rss2json.com/v1/api.json?rss_url=
+    // just add your news rss url to the end of the api
     public static NEWS_RSS_URL = 'newsrssurl';
+    // did the same for sport news
     public static SPORT_NEWS_FUSSBALL_RSS_URL = 'sportnewsrssurl';
 
+    // and the same for quotes rss, i used quotesdayy inspirational quote
     public static QUOTES_RSS_URL = 'quotesrssur';
+
+    // the mapping from OpenWeatherMap icons to weather-icons
+    public static ICON_JSON = '../../assets/icons.json';
 }
